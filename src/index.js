496a2ef313d7ea8b7e46da4bea105de62b78e9d6
Alexandre <acpimentel@gmail.com>
2025-09-06 23:38:41 -0300

    Atualizar o model prisma

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import router from "./routes.js"
+import cors from 'cors'
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use("/api", router)
 
